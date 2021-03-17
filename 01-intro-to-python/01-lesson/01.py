e1282326911d55a0c53b77df41c1d5f181e2c0a1
Peter Golov <peter@golov.me>
2021-03-17 22:30:35 +0300

    Урок 1. Знакомство с Python

---
+++ 01-intro-to-python/01-lesson/01.py
@@ -0,0 +1,7 @@
+print("--------------- 1 ---------------")
+
+name = input('What is your name? ')
+age = input('How old are you? ')
+
+print(f"Hello, {name}!")
+print(f"You are {age} years old.")
---
+++ 01-intro-to-python/01-lesson/03.py
@@ -0,0 +1,7 @@
+print("--------------- 3 ---------------")
+
+i_string = input('Enter an integer: ')
+
+total = int(i_string) + int(i_string + i_string) + int(i_string + i_string + i_string)
+
+print(f"n + nn + nnn = {total}")
